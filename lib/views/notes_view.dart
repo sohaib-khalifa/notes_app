3e9e3c2ffb9f1138b8f2fdf810cc6dd8e2207552
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2025-11-30 23:31:15 +0200

    create fetch note method

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -7,25 +9,28 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.cyanAccent,
-        // backgroundColor: const Color(0xFF4EE4C1),
-        shape: CircleBorder(),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add, color: Colors.black),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.cyanAccent,
+          // backgroundColor: const Color(0xFF4EE4C1),
+          shape: CircleBorder(),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add, color: Colors.black),
+        ),
       ),
     );
   }
